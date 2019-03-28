c87e97599b4a6d32e561eb5740fc143c7268d9c7
Simson L. Garfinkel <simsong@acm.org>
2019-03-28 08:55:49 -0400

    fixed for python2

--- lab4/sql_scan.py
+++ lab4/sql_scan.py
@@ -11,7 +11,46 @@ MIN_SIZE = 4096
 MAGIC = b"SQLite format 3"
 NOTIFY_INTERVAL=1000
 
-import tydoc
+class QuickTable:
+    def __init__(self):
+        self.data = []
+
+    def col(self,n):
+        """Return column n as a list"""
+        return [(row[n] if n<=len(row) else None) for row in self.data]
+
+    def cols(self):
+        """Return the maximum number of columns in the table."""
+        return max(len(row) for row in self.data)
+
+    def col_maxwidth(self,n):
+        """Return the maximum with of column n"""
+        return max( [len(e) for e in self.col(n)] )
+
+    def add_head(self,row):
+        self.data.append(row)
+
+    def add_data(self,row):
+        self.data.append(row)
+
+    def save(self,f, format='md'):
+        """Get the maximum width of each column and then typset each. Assumes one header."""
+        cols = self.cols()      # it's expensive to calculate, so cache it here
+        widths = [self.col_maxwidth(n) for n in range(cols)]
+        fmt = "|" + "|".join([('{:' + repr(width) + '}') for width in widths]) + "|"
+        for (ctr,row) in enumerate(self.data):
+            # Pad this row out if it needs padding
+            if len(row) < cols:
+                row = row + [''] * (cols-len(row))
+            f.write(fmt.format(*row))
+            f.write("\n")
+            # Add the lines
+            if ctr==0:
+                lines = ['-' * self.col_maxwidth(n) for n in range(cols)]
+                f.write(fmt.format(*lines))
+                f.write("\n")
+            
+        
 
 def process(root,nrows=5):
     scanned_dirs = 0
@@ -28,22 +67,21 @@ def process(root,nrows=5):
                     continue
                 if open(filepath,"rb").read(len(MAGIC)) != MAGIC:
                     continue
-                print("a")
                 conn = sqlite3.connect(filepath)
-                print(conn)
             except sqlite3.OperationalError as r:
                 continue
-            except PermissionError as e:
+            except Exception as e:
                 print(str(e))
-                if os.getuid()!=0:
-                    raise RuntimeError("Re-run as root")
-            except FileNotFoundError as e:
                 continue
             print("DATABASE FILE: {}".format(filepath))
-            tables = conn.cursor().execute("select name from sqlite_master").fetchall()
+            try:
+                tables = conn.cursor().execute("select name from sqlite_master").fetchall()
+            except sqlite3.OperationalError as e:
+                print("Could not access.")
+                continue
             # Now print each table and a few rows
             for (name,) in tables:
-                doc = tydoc.tytable()
+                doc = QuickTable()
                 c = conn.cursor()
                 try:
                     c.execute("SELECT COUNT(*) from {}".format(name))
@@ -53,7 +91,7 @@ def process(root,nrows=5):
                     c.execute("SELECT * from {} LIMIT {}".format(name,nrows))
                     doc.add_head([info[0] for info in c.description])
                     for row in c.fetchall():
-                        doc.add_data([str(s)[0:20] for s in row])
+                        doc.add_data([repr(s)[0:20] for s in row])
                     doc.save(sys.stdout, format='md')
                     print("")
                     print("=====================================")
